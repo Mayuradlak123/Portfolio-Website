7bc70afbd6ee163c296edae3e8003083b3142d6a
Mayur Adlak <mayuradlak030@gmail.com>
2024-02-03 23:56:50 +0530

    m4jkgs

--- src/Components/pages/Education.jsx
+++ src/Components/pages/Education.jsx
@@ -1,7 +1,5 @@
 // Education.js
 import React from 'react';
-
-
 const Education = () => {
   return (
     <section id="education" className="bg-gray-100 py-16">
@@ -9,7 +7,7 @@ const Education = () => {
         <h2 className="text-3xl font-semibold mb-8 text-center">Education</h2>
         
         {/* Education items */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
           {/* Replace this with your actual education details */}
           <div className="bg-white p-6 rounded-md shadow-md ">
             <h3 className="text-xl font-semibold mb-2">Oriental University Indore</h3>
@@ -26,12 +24,7 @@ const Education = () => {
             <p className="text-gray-600">Passing Year: 2020</p>
           </div>
 
-          <div className="bg-white p-6 rounded-md shadow-md">
-            <h3 className="text-xl font-semibold mb-2">Achivement </h3>
-            <p className="text-gray-600"> HackerRank : <Rating name="read-only" value={5} readOnly /> </p>
-            <p className="text-gray-600"> Hackerearth : <Rating name="read-only" value={4.5} readOnly /> </p>
-            <p className="text-gray-600">  </p>
-          </div>
+       
 
           {/* Add more education items as needed */}
 
