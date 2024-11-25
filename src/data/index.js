b5bcd591caa0bd00797032c67a946768623a830c
Mayur Adlak <mayuradlak030@gmail.com>
2024-11-25 12:51:44 +0530

    Updated Profile

--- src/data/index.js
+++ src/data/index.js
@@ -1,21 +1,36 @@
-export  const experiences = [
-  
-    {
-      company: "38 Labs Private Limited",
-      role: "Software Developer Intern",
-      duration: "1 Year (Jan 22 - Dec 22)",
-      description: `Developed and deployed full-stack web applications using Python Django, React js, Node js, SQL, and AWS. Leveraged AWS services, such as EC2 and S3, to deploy and manage applications in a cloud.Through collaboration with a diverse team, I learned agile development methodologies, code review practices. Created CI & CD Pipelines for automate the process of testing and deployment using Jenkins.`,
-    },
-    {
-      company: "WebSeeder Technology",
-      role: " Full Stack Developer Intern",
-      duration: "6 Months (Jan 23 - June 23)",
-      description: ` I gained valuable experience in full-stack web development with a focus on the MERN stack and AWS cloud services. By utilizing MongoDB, Express.js, React, and Node.js, I built dynamic applications  with server-side logic.Utilized Git for version control and collaborated with the team on branching and merging strategies. Implemented the Google Authentication and Payment Gateway using Stripe. `,
-    },
-    {
-      company: "Logixi Technology",
-      role: "Software Developer Intern",
-      duration: "(June 23-Present)",
-      description: ` I leveraged Node.js, Express js to build scalable APIs and utilized SQL databases for efficient data storage and retrieval. Created front-end using React.js and Next js.  With a focus on API and Web application development, I extensively used Postman for testing and debugging to ensure optimal functionality and error handling.ebugged and resolved issues to improve  API performance and reliability .`,
-    },
-  ];
+export const experiences = [
+
+  {
+    company: "38 Labs Private Limited",
+    role: "Software Developer Intern",
+    duration: "6 Months (Jan 22 - June 22)",
+    description: `Worked as a full stack developer intern, exploring React.js and Next.js for frontend development, and 
+Node.js, Express.js, and Django for backend development using SQL and MongoDB databases. Gained 
+experience in cloud technologies using AWS and created web applications for job portals, LMS, CRM, and 
+enterprise software products. `,
+  },
+  {
+    company: "Logixi Technology",
+    role: " Software Developer",
+    duration: "1 Year 6 Months (Jun 22 - De 23)",
+    description: `Gained valuable experience in backend development using Node.js, Express.js, and Laravel PHP tech stacks. 
+Explored cloud services such as AWS (S3, EC2, RDS, Lambda, Amplify) and GCP. Created CI/CD pipelines using 
+AWS and GitHub Action Runner with Node.js and Laravel. Learned about web servers like Nginx and Apache. 
+Developed full-stack microservices Ed-Tech products using React.js, Node.js, and Laravel with MySQL. 
+Improved product performance by implementing indexing to reduce latency and adding a load balancer to 
+distribute traffic across services.  `,
+  },
+  {
+    company: "Trando Technology",
+    role: "Software Developer",
+    duration: "(Jan 24-Present)",
+    description: `  Working as a full stack developer, focusing on React.js and Next.js for frontend development, and deploying 
+web applications on AWS Amplify with CI/CD to automate the deployment and testing process. Utilizing 
+Node.js and Express.js with MySQL and MongoDB databases, I also added a load balancer to increase the 
+performance of backend services, implementing replicas and indexing to improve server efficiency. 
+Integrated push notifications and email communication modules using Firebase and Nodemailer. Created 
+background jobs to run processes in the background. Integrated payment gateways using Razorpay and 
+Stripe with webhooks to ensure payment processing. Currently working on a variety of projects, including 
+Ed-Tech, Fintech, and travel booking service provider web applications. `,
+  },
+];
